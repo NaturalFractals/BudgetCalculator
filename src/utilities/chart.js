8df3d2a61cdbed31f7ed6f43e111cd129debba71
JesseCochran <jcochran@pietech.com>
2017-07-12 12:21:16 -0400

    Added chart.js and dependencies.

---
+++ src/utilities/chart.js
@@ -0,0 +1,13 @@
+import * as HighCharts from "highcharts";
+
+export class Chart {
+    constructor() {
+
+    }
+
+    createChart(containerID) {
+        Highcharts.chart(containerID, {
+            
+        })
+    }
+}
