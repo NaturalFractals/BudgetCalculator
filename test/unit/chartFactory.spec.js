13696bfb33cc41a26d0f54fdf3246335cf3e1bd3
Sean McHugh <seanfmch@gmail.com>
2017-07-17 11:49:34 -0400

    just trying to pull

---
+++ test/unit/chartFactory.spec.js
@@ -0,0 +1,14 @@
+import {ChartFactory} from '../../src/utilities/chartFactory';
+import {MasterBudget} from '../../src/masterBudget';
+
+describe('Testing ChartFactory', () => {
+
+    beforeEach(() => {
+        this.masterBudget = new MasterBudget();
+    });
+
+    it("blah", () = {
+
+    });
+
+});
