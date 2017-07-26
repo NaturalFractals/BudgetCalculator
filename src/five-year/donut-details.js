c6d920fd2f7e5fc79c30875c5a2f499c5b86fee4
Sean McHugh <seanfmch@gmail.com>
2017-07-26 09:42:31 -0400

    Fixed five year page, gray numbers on results page

--- src/five-year/donut-details.js
+++ src/five-year/donut-details.js
@@ -6,47 +6,49 @@ import {ChartFactory} from '../utilities/chartFactory';
 @inject(MasterBudget, EventAggregator)
 export class DonutDetails {
     constructor(masterBudget, eventAggregator) {
+        this.masterBudget = masterBudget;
         this.currentYear = 2017;
-        this.currentExpense = '$5,463';
+        this.currentExpense = this.masterBudget.childCare.cost;
         this.currentExpenseYearCost = 324;
         this.currentCategory = 'Child Care';
-        this.masterBudget = masterBudget;
-        eventAggregator.subscribe("change year", year => {this.changedYear(year)});
+        eventAggregator.subscribe("change year", year => {this.currentYear = year;
+                                                          this.changeNavigationTab(this.currentCategory)});
     }
 
     //Changes the information of the currrent details based on navigation click
-    changeNavigationTab(category, currentCategory) {
+    changeNavigationTab(category) {
+
         switch(category) {
             case this.masterBudget.budgetCategories[0]:
-                this.currentExpense = this.masterBudget.childCare.cost;
+                this.currentExpense = this.masterBudget.childCare.cost + (this.currentYear - 2017) * 1.0558;
                 this.currentCategory = category;
                 break;
             case this.masterBudget.budgetCategories[1]:
-                this.currentExpense = this.masterBudget.food.cost;
+                this.currentExpense = this.masterBudget.food.cost + (this.currentYear - 2017) * 1.0295;
                 this.currentCategory = category;
                 break;
             case this.masterBudget.budgetCategories[2]:
-                this.currentExpense = this.masterBudget.housing.cost;
+                this.currentExpense = this.masterBudget.housing.cost + (this.currentYear - 2017) * 1.0269;
                 this.currentCategory = category;
                 break;
             case this.masterBudget.budgetCategories[3]:
-                this.currentExpense = this.masterBudget.medical.cost;
+                this.currentExpense = this.masterBudget.medical.cost + (this.currentYear - 2017) * 1.0379;
                 this.currentCategory = category;
                 break;
             case this.masterBudget.budgetCategories[4]:
-                this.currentExpense = this.masterBudget.other.cost;
+                this.currentExpense = this.masterBudget.other.cost + (this.currentYear - 2017) * 1;
                 this.currentCategory = category;
                 break;
             case this.masterBudget.budgetCategories[5]:
-                this.currentExpense = this.masterBudget.savings.cost;
+                this.currentExpense = this.masterBudget.savings.cost + (this.currentYear - 2017) * 1;
                 this.currentCategory = category;
                 break;
             case this.masterBudget.budgetCategories[6]:
-                this.currentExpense = this.masterBudget.taxes.cost;
+                this.currentExpense = this.masterBudget.taxes.cost + (this.currentYear - 2017) * 1;
                 this.currentCategory = category;
                 break;
             case this.masterBudget.budgetCategories[7]:
-                this.currentExpense = this.masterBudget.transportation.cost;
+                this.currentExpense = this.masterBudget.transportation.cost + (this.currentYear - 2017) * 1.0329;
                 this.currentCategory = category;
                 break;
             default:
