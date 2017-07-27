9caecc3d944fb8210433327b0e9d5c5197ea3fe2
Sean McHugh <seanfmch@gmail.com>
2017-07-27 14:04:08 -0400

    css

--- src/auto-budget/auto-budget.js
+++ src/auto-budget/auto-budget.js
@@ -1,5 +1,13 @@
-export class AutoBudget {
-    constructor() {
+import { inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
 
+@inject(Router)
+export class AutoBudget {
+    constructor(router) {
+        this.router = router;
+    }
+
+    routeResults() {
+        this.router.navigate("#/results");
     }
 }
